7d97af7841cb921282b58a2c01f0fcdb079d9f2f
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2024-07-11 11:19:29 +0900

    Create Alarm & TempKey Service

---
+++ TaskMaster/src/main/java/com/mysite/alarm/contoller/AlarmController.java
@@ -0,0 +1,37 @@
+package com.mysite.alarm.contoller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mysite.alarm.vo.AlarmVO;
+import com.mysite.alarm.service.AlarmService;
+
+@Controller
+public class AlarmController {
+
+    @Autowired
+    AlarmService alarmService;
+
+    @RequestMapping("/alarmTest")
+    public String alarm() throws Exception {
+        return "/alarmTest";
+    }
+
+    @RequestMapping(value = "/alarm_test", method = RequestMethod.POST)
+    @ResponseBody
+	public String alarm_test(AlarmVO vo) throws Exception {
+        return alarmService.createAlarm(vo);
+	}
+
+    @RequestMapping(value = "/getAlarm_test", method = RequestMethod.POST)
+    @ResponseBody
+	public List<AlarmVO> alarm_test(@RequestParam("userId") String userId) throws Exception {
+        return alarmService.getAlarm(userId);
+	}
+}
---
+++ TaskMaster/src/main/java/com/mysite/alarm/service/AlarmService.java
@@ -0,0 +1,15 @@
+package com.mysite.alarm.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.mysite.alarm.vo.AlarmVO;
+
+@Service
+public interface AlarmService {
+    
+    String createAlarm(AlarmVO vo) throws Exception;
+
+    List<AlarmVO> getAlarm(String userId);
+}
